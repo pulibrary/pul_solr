309f6944bcfce457f9ccad5b93e948ac572ccf2f
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-04-20 17:08:36 -0400

    update "0" and qutation marks

--- spec/orangelight/cjk_mapping_spec.rb
+++ spec/orangelight/cjk_mapping_spec.rb
@@ -4,7 +4,7 @@ require 'json'
 docs = JSON.parse(File.read('spec/fixtures/cjk_map_solr_fixtures.json'))
 
 describe 'CJK character equivalence' do
-  def add_single_char_doc char
+  def add_single_field_doc char
     @@solr.add({ id: 1, cjk_title: char })
     @@solr.commit
   end
@@ -31,37 +31,47 @@ describe 'CJK character equivalence' do
     end
   end
   describe '4 character variations 壱 => 一, 壹 => 一, 弌 => 一' do
-    before(:all) do
-      delete_all
-    end
     it 'indirect mapping 壹 => 壱' do
-      add_single_char_doc('壹')
+      add_single_field_doc('壹')
       expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:壱' })).to include('1')
     end
     it 'indirect mapping 壹 => 弌' do
-      add_single_char_doc('弌')
+      add_single_field_doc('弌')
       expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:壹' })).to include('1')
     end
     it 'indirect mapping 壱 => 弌' do
-      add_single_char_doc('壱')
+      add_single_field_doc('壱')
       expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:弌' })).to include('1')
     end
   end
   describe 'multi-character searches' do
-    before(:all) do
-      delete_all
-    end
     it '毛泽东思想 => 毛澤東思想' do
-      add_single_char_doc('毛泽东思想')
+      add_single_field_doc('毛泽东思想')
       expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:"毛澤東思想"' })).to include('1')
     end
     it '毛澤東思想 => 毛沢東思想' do
-      add_single_char_doc('毛澤東思想')
+      add_single_field_doc('毛澤東思想')
       expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:"毛沢東思想"' })).to include('1')
     end
     it '毛沢東思想 => 毛泽东思想' do
-      add_single_char_doc('毛沢東思想')
+      add_single_field_doc('毛沢東思想')
       expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:"毛泽东思想"' })).to include('1')
+    end
+  end
+  describe 'punctuation marks are stripped' do
+    it '『「｢「想』」｣」 => 」｣想｢「' do
+      add_single_field_doc('『「｢「想』」｣」')
+      expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:"」｣想｢「"' })).to include('1')
+    end
+  end
+  describe 'cjk "0"' do
+    it '二〇〇〇 => 二０００' do
+      add_single_field_doc('二０００')
+      expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:"二〇〇〇"' })).to include('1')
+    end
+    it '二０００ => 二〇〇〇' do
+      add_single_field_doc('二〇〇〇')
+      expect(solr_resp_doc_ids_only({ 'fq'=>'cjk_title:"二０００"' })).to include('1')
     end
   end
   after(:all) do
