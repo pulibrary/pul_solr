778dd9bc0a5c844945785df0aebe5474310215f2
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-06-20 18:17:45 -0400

    prevents punctuation marks surrounded by whitespace to affect mm

---
+++ spec/orangelight/punctuation_tokens_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'json'
+
+describe 'stripping punctuation surrounded by whitespace' do
+  def qf_pf_string q, field
+    "{!qf=$#{field}_qf pf=$#{field}_pf}#{q}"
+  end
+  before(:all) do
+    delete_all
+    @china_and_angola = '7196990'
+    add_doc(@china_and_angola)
+  end
+  describe 'default text query' do
+    it 'matches when colon is included at end of query word' do
+      expect(solr_resp_doc_ids_only({ 'q' => 'China and Angola: marriage convenience' })).to include(@china_and_angola)
+    end
+    it 'matches when colon is surrounded by whitespace' do
+      expect(solr_resp_doc_ids_only({ 'q' => 'China and Angola : marriage convenience' })).to include(@china_and_angola)
+    end
+  end
+  describe 'title query' do
+    it 'matches when colon is included at end of query word' do
+      expect(solr_resp_doc_ids_only({ 'q' => qf_pf_string('China and Angola: marriage convenience',
+                                                          'title') })).to include(@china_and_angola)
+    end
+    it 'matches when colon is surrounded by whitespace' do
+      expect(solr_resp_doc_ids_only({ 'q' => qf_pf_string('China and Angola : marriage convenience',
+                                                          'title') })).to include(@china_and_angola)
+    end
+  end
+  describe 'author query' do
+    it 'matches when exclamation point is included at end of query word' do
+      expect(solr_resp_doc_ids_only({ 'q' => qf_pf_string('Power! Marcus',
+                                                          'author') })).to include(@china_and_angola)
+    end
+    it 'matches when exclamation point is surrounded by whitespace' do
+      expect(solr_resp_doc_ids_only({ 'q' => qf_pf_string('Power ! Marcus',
+                                                          'author') })).to include(@china_and_angola)
+    end
+  end
+  describe 'subject query' do
+    it 'matches when undesired character is included at end of query word' do
+      expect(solr_resp_doc_ids_only({ 'q' => qf_pf_string('foreign economic relations% angola',
+                                                          'subject') })).to include(@china_and_angola)
+    end
+    it 'matches when undesired character is surrounded by whitespace' do
+      expect(solr_resp_doc_ids_only({ 'q' => qf_pf_string('foreign economic relations % angola',
+                                                          'subject') })).to include(@china_and_angola)
+    end
+  end
+  after(:all) do
+    delete_all
+  end
+end
