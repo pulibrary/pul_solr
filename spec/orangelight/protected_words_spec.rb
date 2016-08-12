195dbbc5615a13733ef04331f07f021834f7362c
Eliot Jordan <eliot.jordan@gmail.com>
2016-08-12 06:16:51 -0500

    add words to protected word file (#15)

---
+++ spec/orangelight/protected_words_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'json'
+
+describe 'protected words' do
+  before(:all) do
+    delete_all
+  end
+  context 'when performing a keyword search using a protected word' do
+    not_stemmed = '2393109'
+    stemmed = '1261826'
+    before(:all) do
+      add_doc(not_stemmed)
+      add_doc(stemmed)
+    end
+    it 'does not stem the word' do
+      expect(solr_resp_doc_ids_only({ 'q' => 'constanter' })).to include(not_stemmed)
+      expect(solr_resp_doc_ids_only({ 'q' => 'constanter' })).not_to include(stemmed)
+    end
+  end
+  after(:all) do
+    delete_all
+  end
+end
