1017f00267665fa77b7d6469f52a29a6b40ae9e6
Anna Headley <anna.headley@gmail.com>
2020-01-16 14:47:05 -0500

    Add blank search urls to urls.txt

--- scripts/generate_seige_urls.rb
+++ scripts/generate_seige_urls.rb
@@ -10,13 +10,26 @@ ADJ_DICT = File.join(__dir__, "generate_seige_urls", "adjectives.txt")
 OUTFILE = "urls.txt"
 
 class UrlFileGenerator
-  def write_url_file(lines: 100)
+  # Specify number of lines for the file.
+  # Half the lines will be randomly-generated searches; half will be
+  # blank searches.
+  # Siege selects randomly from the file if invoked with the -i option.
+  def write_url_file(lines: 1_000_000)
+    half = lines / 2
     File.open(OUTFILE, 'w') do |f|
-      lines.times do
+      half.times do
         f.write generate_url
         f.write "\n"
       end
+      half.times do
+        f.write blank_search_url
+        f.write "\n"
+      end
     end
+  end
+
+  def blank_search_url
+    "https://catalog-staging.princeton.edu/catalog?utf8=%E2%9C%93&search_field=all_fields&q="
   end
 
   def generate_url
