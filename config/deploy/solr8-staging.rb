748b91c5ab008f16d4b2b78b0c822c22881602dd
Anna Headley <anna.headley@gmail.com>
2020-02-20 13:15:07 -0500

    Update solr8staging to use new zookeeper cluster

--- config/deploy/solr8-staging.rb
+++ config/deploy/solr8-staging.rb
@@ -3,7 +3,7 @@ server 'lib-solr-staging4', user: 'deploy', roles: %{main}
 set :branch, ENV['BRANCH'] || 'master'
 
 def zk_host
-  "lib-zk-staging1:2181,lib-zk-staging2:2181,lib-zk-staging3:2181/solr8"
+  "lib-zk8-staging1:2181,lib-zk8-staging2:2181,lib-zk8-staging3:2181/solr8"
 end
 
 def config_map
