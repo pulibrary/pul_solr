60cae75f1cfb530f828d62373c1ef3664d440d11
Khanh Vu <kvu@princeton.edu>
2020-03-02 16:54:14 -0500

    began tests for container number search

--- spec/pulfalight/identifier_spec.rb
+++ spec/pulfalight/identifier_spec.rb
@@ -35,24 +35,41 @@ describe 'all fields search for id value' do
     add_doc(mc_subgroup_name)
     add_doc(mc_series_name)
 
-    add_doc(mc_component_name1)   # c03575
-    add_doc(mc_component_name2)   # c03585
-    add_doc(mc_component_name3)   # c03750
+    add_doc(mc_component_name1)   # c03575, "box 1865", "folder 1"
+    add_doc(mc_component_name2)   # c03585, “box 1866”, “folder 1”
+    add_doc(mc_component_name3)   # c03750, “box 1865”, “folder 2”
   end
 
   describe 'box numbers and folder numbers' do
     it 'Do a search with the collection ID, and container information at same time (ex: collection ID, box # or collection ID, box # folder #)' do
-      puts query_params('MC001-02-04MC001-02-04_c03575').merge('fl' => 'id', 'facet' => 'false')
-      response = solr_response(query_params('MC001-02-04MC001-02-04_c03575').merge('fl' => 'id', 'facet' => 'false'))
-      byebug
+      response = solr_response(query_params('box 1865').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).to include(mc_component_name1)
+      expect(response.to_s).not_to include(mc_component_name2)
+      expect(response.to_s).to include(mc_component_name3)
 
-      response = solr_response(query_params('box 1').merge('fl' => 'id', 'facet' => 'false'))
-      byebug
+      response = solr_response(query_params('folder 1').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).to include(mc_component_name1)
-      # expect(response.to_s).to include(tc_collection_name)
-      # expect(response.to_s).to include(tc_component_name)
-      # expect(response.to_s).not_to include("\"#{bad_tc_collection_name}\"")
+      expect(response.to_s).to include(mc_component_name2)
+      expect(response.to_s).not_to include(mc_component_name3)
+
+      # case insensitive 
+      response = solr_response(query_params('FOLDER 1').merge('fl' => 'id', 'facet' => 'false'))
+      expect(response.to_s).to include(mc_component_name1)
+      expect(response.to_s).to include(mc_component_name2)
+      expect(response.to_s).not_to include(mc_component_name3)
+
+      # response = solr_response(query_params('box 1865 folder 2').merge('fl' => 'id', 'facet' => 'false'))
+      # byebug
+      # expect(response.to_s).not_to include(mc_component_name1)
+      # expect(response.to_s).not_to include(mc_component_name2)
+      # expect(response.to_s).to include(mc_component_name3)
+
+      # response = solr_response(query_params('MC001-02-04MC001-02-04_c03575 box 1865').merge('fl' => 'id', 'facet' => 'false'))
+      # byebug
+      # expect(response.to_s).to include(mc_component_name1)
+      # expect(response.to_s).not_to include(mc_component_name2)
+      # expect(response.to_s).not_to include(mc_component_name3)
+
     end
   end
 
@@ -60,7 +77,6 @@ describe 'all fields search for id value' do
   describe 'collection and component ids' do
 
     it 'retrieves both collection and component when search for collection_id' do
-      puts query_params('TC071').merge('fl' => 'id', 'facet' => 'false')
       response = solr_response(query_params('TC071').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).to include(tc_collection_name)
       expect(response.to_s).to include(tc_component_name)
@@ -68,44 +84,37 @@ describe 'all fields search for id value' do
     end
 
     it 'tests case insensitivity' do
-      puts query_params('tc071').merge('fl' => 'id', 'facet' => 'false')
       response = solr_response(query_params('tc071').merge('fl' => 'id', 'facet' => 'false'))
-      byebug
       expect(response.to_s).to include(tc_collection_name)
       expect(response.to_s).to include(tc_component_name)
       expect(response.to_s).not_to include("\"#{bad_tc_collection_name}\"")
     end
 
     it 'retrieves only component when search for component_id' do
-      puts query_params('TC071TC071_c00001').merge('fl' => 'id', 'facet' => 'false')
       response = solr_response(query_params('TC071TC071_c00001').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).to include(tc_component_name)
       expect(response.to_s).not_to include("\"#{tc_collection_name}\"")
     end
 
     it 'retrieves component even when search for incomplete component id' do
-      puts query_params('TC071_c00001').merge('fl' => 'id', 'facet' => 'false')
       response = solr_response(query_params('TC071_c00001').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).to include(tc_component_name)
       # expect(response.to_s).not_to include("\"#{tc_collection_name}\"")
     end
 
     it 'does not retrieve collection when search for TC 071' do
-      puts query_params('TC 071').merge('fl' => 'id', 'facet' => 'false')
       response = solr_response(query_params('TC 071').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).not_to include("\"#{tc_collection_name}\"")
       expect(response.to_s).not_to include("\"#{bad_tc_collection_name}\"")
     end
 
     it 'retrieves only subgroup and not series when search for series id' do
-      puts query_params('MC001.04').merge('fl' => 'id', 'facet' => 'false')
       response = solr_response(query_params('MC001.04').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).to include("\"#{mc_subgroup_name}\"")
       expect(response.to_s).not_to include("\"#{mc_series_name}\"")
     end
 
     it 'retrieves only collection and neither subgroup nor series when search for collection id' do
-      puts query_params('MC001').merge('fl' => 'id', 'facet' => 'false')
       response = solr_response(query_params('MC001').merge('fl' => 'id', 'facet' => 'false'))
       expect(response.to_s).to include("\"#{mc_collection_name}\"")
       expect(response.to_s).not_to include("\"#{mc_subgroup_name}\"")
