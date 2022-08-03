541459ed86659e254c6fbd46683fa6b49f5a1fca
Anna Headley <anna.headley@gmail.com>
2022-08-03 13:08:19 -0400

    Set `staging` and `production` deploy targets to solr 8 boxes
    closes #324

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,71 +1,29 @@
-server 'lib-solr-staging1', user: 'deploy', roles: %{main}
+server 'lib-solr-staging4', user: 'deploy', roles: %{main}
 
-set :whenever_host, ->{ "solr7" }
+set :whenever_host, ->{ "solr8" }
 set :whenever_environment, ->{ "staging" }
 
 def zk_host
-  "lib-zk-staging1:2181,lib-zk-staging2:2181,lib-zk-staging3:2181/solr7"
+  "lib-zk-staging1:2181,lib-zk-staging2:2181,lib-zk-staging3:2181/solr8"
 end
 
+# config directory => config set name
 def config_map
   {
-    "figgy" => "figgy",
-    "pulmap" => "pulmap",
+    "catalog-production-v2" => "catalog-production-v2",
+    "dss-production" => "dss-production",
+    "pulfalight-staging" => "pulfalight-staging",
+    "cicognara" => "cicognara",
+    "lae" => "lae",
+    "dpul" => "dpul",
+    "pdc-discovery" => "pdc-discovery-staging",
+    "oawaiver" => "oawaiver-staging",
+    "figgy" => "figgy-staging",
     "libwww" => "libwww",
+    "pulmap" => "pulmap"
   }
 end
 
 def collections
-  PulSolr.collections["solr7_staging"]
+  PulSolr.collections["solr8_staging"]
 end
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
