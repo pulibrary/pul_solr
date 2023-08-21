f10b496ce1f95bb012be18b26a1182b965ae88f7
Jane Sandberg <js7389@princeton.edu>
2023-08-21 12:33:36 -0700

    Take JSON DSL query from Orangelight when configured to use JSON DSL
    Also, upgrade rsolr
    
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

--- spec/orangelight/advanced_search_spec.rb
+++ spec/orangelight/advanced_search_spec.rb
@@ -1,35 +1,42 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'json'
 
 describe 'subject keyword search' do
   include_context 'solr_helpers'
-  
+
   before do
     delete_all
-    add_doc("212556")
-    add_doc("454035")
-    add_doc("1225885")
+    add_doc('212556')
+    add_doc('454035')
+    add_doc('1225885')
     solr.commit
   end
 
   describe 'advanced search dsl query' do
-    let(:response) { solr_resp_doc_ids_only(params)['response'] }
+    let(:response) do
+      RSpecSolr::SolrResponseHash.new(solr.send_and_receive('advanced', params))['response']
+    end
     let(:docs) { response['docs'] }
     let(:params) do
       { data:
-        {
-        "query":
-          {"bool":
-            {"must":[
-              # {"edismax":{"spellcheck.dictionary":"title","qf":"$title_qf","pf":"$title_pf","query":"Theory of the avant-garde"}},
-              {"edismax":{"spellcheck.dictionary":"author","qf":"author_display","pf":"author_display","query":"Schumpeter"}}
-            ]}
-          }
-        }
-      }
+        '{"params":'\
+          '{"qt":null,"spellcheck.dictionary":"title",'\
+           '"qf":"${title_qf}","pf":"${title_pf}",'\
+           '"facet":true,'\
+           '"f.access_facet.facet.sort":"index","f.location.facet.sort":"index",'\
+           '"f.location.facet.mincount":1,"f.location.facet.limit":21,'\
+           '"facet.pivot":["lc_1letter_facet,lc_rest_facet"],"f.sudoc_facet.facet.sort":"index",'\
+           '"f.sudoc_facet.facet.limit":11,"rows":20,"sort":"score desc, pub_date_start_sort desc, title_sort asc",'\
+           '"stats":"true","stats.field":["pub_date_start_sort"],"q":null},'\
+          '"query":{"bool":{"must":['\
+            '{"edismax":{"spellcheck.dictionary":"title","qf":"${title_qf}","pf":"${title_pf}","query":"Theory of the avant-garde"}}]}}}',
+        method: :post, headers: { 'Content-Type' => 'application/json' } }
     end
     it 'returns one document that matches the boolean query' do
-      expect(response["docs"]).to eq([{ "id" => "212556" }])
+      expect(response['docs'].length).to eq(1)
+      expect(response['docs'].first['id']).to eq('212556')
     end
   end
 end
