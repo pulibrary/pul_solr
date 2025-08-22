73abfe63da62406164a38a841b1dba36aebe4c91
Ryan Laddusaw <rladdusaw@princeton.edu>
2025-08-22 14:53:22 -0400

    Revert to using localhost

--- lib/pul_solr.rb
+++ lib/pul_solr.rb
@@ -3,11 +3,10 @@ require 'yaml'
 
 module PulSolr
   def self.solr_connection
-    host = ENV['SOLR_URL'] == "http://solr8:8983/solr" ? "solr:SolrRocks@solr8" : "solr:SolrRocks@solr9"
-    port = ENV['SOLR_URL'] == "http://solr8:8983/solr" ? "8983" : "8984"
+    port = ENV['SOLR_URL'] == "http://localhost:8983/solr" ? "8983" : "8984"
     @@solr_connection ||= {
       test: {
-        host: ENV['CI'] ? host : "localhost",
+        host: ENV['CI'] ? "solr:SolrRocks@localhost" : "localhost",
         catalog: {
           port: ENV['CI'] ? port : ENV['lando_blacklight_test_solr_conn_port'],
           core: "solr/blacklight-core",
