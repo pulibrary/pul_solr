932250bd9f1d4f5064140beb7c9c80baccccf354
Ryan Laddusaw <rladdusaw@princeton.edu>
2025-08-22 14:27:59 -0400

    Use alternate port for zookeeper

--- docker/ci-start.sh
+++ docker/ci-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 bin/solr start --cloud --no-prompt -p 8984
-server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9983 -cmd putfile /security.json /opt/solr/security.json
+server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9984 -cmd putfile /security.json /opt/solr/security.json
 sleep infinity
