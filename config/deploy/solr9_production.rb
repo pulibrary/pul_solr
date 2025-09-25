73628fd273e0321e86c8cc9b7965d5c721054493
Christina Chortaria <actspatial@gmail.com>
2025-09-25 13:43:42 -0400

    [Solr9] remove /solr9 from zk_host
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- config/deploy/solr9_production.rb
+++ config/deploy/solr9_production.rb
@@ -4,7 +4,7 @@ set :whenever_host, ->{ "solr9" }
 set :whenever_environment, ->{ "production" }
 
 def zk_host
-  "lib-zk4:2181,lib-zk5:2181,lib-zk6:2181/solr9"
+  "lib-zk4:2181,lib-zk5:2181,lib-zk6:2181"
 end
 
 # config directory => config set name
