143b8406a71db4be19841d0a88d8493f75e8d293
Trey Pendragon <tpendragon@princeton.edu>
2026-01-27 14:10:17 -0800

    ZK port can change

--- docker/ci-start.sh
+++ docker/ci-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SOLR_PORT=${SOLR_PORT:=8984}
+export ZK_PORT=${SOLR_PORT:=9984}
 bin/solr start --cloud --no-prompt -p $SOLR_PORT
-server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9984 -cmd putfile /security.json /opt/solr/security.json
+server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:$ZK_PORT -cmd putfile /security.json /opt/solr/security.json
 sleep infinity
