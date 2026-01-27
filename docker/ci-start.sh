f4d1e2bf2657af18e117eb659f6cb72784a2614a
Trey Pendragon <tpendragon@princeton.edu>
2026-01-27 14:09:00 -0800

    Keep zkhost constant

--- docker/ci-start.sh
+++ docker/ci-start.sh
@@ -2,5 +2,5 @@
 
 export SOLR_PORT=${SOLR_PORT:=8984}
 bin/solr start --cloud --no-prompt -p $SOLR_PORT
-server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:$SOLR_PORT -cmd putfile /security.json /opt/solr/security.json
+server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9984 -cmd putfile /security.json /opt/solr/security.json
 sleep infinity
