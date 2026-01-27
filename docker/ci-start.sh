023fa5749fffdc9487ce7c077fb34d231bd7433a
Trey Pendragon <tpendragon@princeton.edu>
2026-01-27 14:16:17 -0800

    Typo

--- docker/ci-start.sh
+++ docker/ci-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export SOLR_PORT=${SOLR_PORT:=8984}
-export ZK_PORT=${SOLR_PORT:=9984}
+export ZK_PORT=${ZK_PORT:=9984}
 bin/solr start --cloud --no-prompt -p $SOLR_PORT
 server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:$ZK_PORT -cmd putfile /security.json /opt/solr/security.json
 sleep infinity
