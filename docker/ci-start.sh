593d7aced9414fc5f63c5d1f70d77b602a03679e
Trey Pendragon <tpendragon@princeton.edu>
2026-01-27 13:48:30 -0800

    Make solr port configurable

--- docker/ci-start.sh
+++ docker/ci-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-bin/solr start --cloud --no-prompt -p 8984
-server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9984 -cmd putfile /security.json /opt/solr/security.json
+export SOLR_PORT=${SOLR_PORT:=8984}
+bin/solr start --cloud --no-prompt -p $SOLR_PORT
+server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:$SOLR_PORT -cmd putfile /security.json /opt/solr/security.json
 sleep infinity
