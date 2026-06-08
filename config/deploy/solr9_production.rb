24d2b142bb46fed5d6c98a1e387939ee42cf16cd
Carolyn Cole <cac9@princeton.edu>
2026-06-08 10:27:55 -0400

    [PDC Dsicovery] updating solr9 to include pdc-discovery for production

--- config/deploy/solr9_production.rb
+++ config/deploy/solr9_production.rb
@@ -16,6 +16,7 @@ def config_map
     "dpulc-production" => "dpulc-production",
     "dss-production" => "dss-production",
     "figgy" => "figgy-9",
+    "pdc-discovery" => "pdc-discovery-production",
     "pulfalight-production" => "pulfalight-production",
     "pulmap" => "pulmap",
     "lae" => "lae"
