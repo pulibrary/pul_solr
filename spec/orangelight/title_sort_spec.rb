147ee3f8deb1725584ecf636f611d26dd031159b
Jane Sandberg <js7389@princeton.edu>
2026-08-05 06:31:36 -0700

    Use an ICU sort key for title sorting, rather than a string
    Helps with #3404
    
    We confirmed that, given the same data indexed into a collection, the `title_sort` adds substantial data to the field cache, while `title_sort_key` does not add any data to the field cache.
    
    The tradeoffs that I expect:
    - This will take a lot less memory than the previous implementation
      (the sort key is very compact, and this uses docValues)
    - This may provide more correct ordering for non-English titles
    - This may take longer to index
    
    For backwards compatibility, this commit includes both the new
    sortkey and the old title_sort field.  The next steps would be:
    
    * Full reindex
    * Change orangelight to look at `title_sort_key` rather than `title_sort`
    * Change bibdata to index directly into `title_sort_key` rather than
      `title_sort`
    * Remove all references to `title_sort` from pul solr
    
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: regineheberlein <regineheberlein@users.noreply.github.com>

--- spec/orangelight/title_sort_spec.rb
+++ spec/orangelight/title_sort_spec.rb
@@ -7,26 +7,26 @@ RSpec.describe 'title sort' do
   before { delete_all }
 
   it 'sorts titles that start with diacritics' do
-    solr.add({id: 1, title_sort: 'Falsche Freunde'})
-    solr.add({id: 2, title_sort: 'Im Morgenrot '})
-    solr.add({id: 3, title_sort: 'Üble Sache Maloney!'})
+    solr.add({id: 1, title_sort_key: 'Falsche Freunde'})
+    solr.add({id: 2, title_sort_key: 'Im Morgenrot '})
+    solr.add({id: 3, title_sort_key: 'Üble Sache Maloney!'})
     solr.commit
 
     expect(
-      solr_response({q: '*', sort: 'title_sort ASC'})['response']['docs']
+      solr_response({q: '*', sort: 'title_sort_key ASC'})['response']['docs']
         .map {|doc| doc['id']}
     ).to eq ['1', '2', '3']
   end
 
   it 'sorts titles without regard to capitalization' do
-    solr.add({id: 4, title_sort: 'Xyz'})
-    solr.add({id: 3, title_sort: 'xy'})
-    solr.add({id: 2, title_sort: 'abd'})
-    solr.add({id: 1, title_sort: 'ABC'})
+    solr.add({id: 4, title_sort_key: 'Xyz'})
+    solr.add({id: 3, title_sort_key: 'xy'})
+    solr.add({id: 2, title_sort_key: 'abd'})
+    solr.add({id: 1, title_sort_key: 'ABC'})
     solr.commit
 
     expect(
-      solr_response({q: '*', sort: 'title_sort ASC'})['response']['docs']
+      solr_response({q: '*', sort: 'title_sort_key ASC'})['response']['docs']
         .map {|doc| doc['id']}
     ).to eq ['1', '2', '3', '4']
   end
